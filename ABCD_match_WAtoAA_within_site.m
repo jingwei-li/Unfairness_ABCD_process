1803c22a5bf64ed0274783f6bd8adb51a5af194d
jingwei-li <allhappyliferay@gmail.com>
2020-07-19 04:52:51 +0800

    Initially add functions to match WA to AA and split folds
    Why:
    
    *
    
    This change addresses the need by:
    
    *

---
+++ ABCD_match_WAtoAA_within_site.m
@@ -0,0 +1,85 @@
+function [selAA, selWA, sel_mAA, sel_mWA] = ABCD_match_WAtoAA_within_site(subjects, race, site, bhvr_zn, cfds_zn, niter)
+
+WArace = 1;
+AArace = 2;
+cost_ceil = 2.5;
+
+AA = subjects(race == AArace);
+AA_sites = site(race == AArace);
+uq_AA_sites = unique(AA_sites);
+
+selAA = cell(size(bhvr_zn,2), length(uq_AA_sites));
+selWA = cell(size(bhvr_zn,2), length(uq_AA_sites));
+sel_mAA = cell(size(bhvr_zn,2), 1);
+sel_mWA = cell(size(bhvr_zn,2), 1);
+for b = 1:size(bhvr_zn,2)
+    for st = 1:length(uq_AA_sites)
+        possib_WA_idx = race == WArace & site == uq_AA_sites(st);
+        
+        curr_AA = AA(AA_sites==uq_AA_sites(st));
+        curr_WA = subjects(possib_WA_idx)';
+        
+        if(~isempty(curr_WA))
+            [~, curr_AAidx] = intersect(subjects, curr_AA, 'stable');
+            [~, curr_WAidx] = intersect(subjects, curr_WA, 'stable');
+            mAA = [cfds_zn(curr_AAidx, :) bhvr_zn(curr_AAidx, b)];
+            mWA = [cfds_zn(curr_WAidx, :) bhvr_zn(curr_WAidx, b)];
+            
+            mAA_3d = reshape(mAA, size(mAA,1), 1, size(mAA,2));
+            mWA_3d = reshape(mWA, 1, size(mWA,1), size(mWA,2));
+            cost_mat = bsxfun(@minus, mAA_3d, mWA_3d);
+            cost_mat = sum(abs(cost_mat),3);
+            
+            cost_last_iter = 1e4;
+            curr_AA_newiter = curr_AA;
+            mAA_newiter = mAA;
+            for iter = 1:niter
+                [asgn_WAidx_newiter, cost_new_iter] = munkres(cost_mat);
+                while(any(asgn_WAidx_newiter==0))
+                    unmatch_idx = find(asgn_WAidx_newiter==0);
+                    curr_AA_newiter(unmatch_idx) = [];
+                    mAA_newiter(unmatch_idx,:) = [];
+                    cost_mat(unmatch_idx, :) = [];
+                    [asgn_WAidx_newiter, cost_new_iter] = munkres(cost_mat);
+                end
+                
+                cost_new_iter = cost_new_iter / length(curr_AA_newiter);
+                cost_idx = sub2ind(size(cost_mat), 1:1:length(curr_AA_newiter), asgn_WAidx_newiter);
+                cost_currAA = cost_mat(cost_idx);
+                [max_cost, maxidx] = max(cost_currAA);
+                
+                cost_diff = cost_last_iter - cost_new_iter;
+                if(cost_diff / cost_new_iter <= 0.05 && max_cost<=cost_ceil)
+                    break
+                end
+                
+                curr_AA = curr_AA_newiter;
+                asgn_WAidx = asgn_WAidx_newiter;
+                mAA = mAA_newiter;
+                cost_last_iter = cost_new_iter;
+                
+                curr_AA_newiter(maxidx) = [];
+                cost_mat(maxidx,:) = [];
+                mAA_newiter(maxidx,:) = [];
+                if(length(curr_AA) == 1)
+                    if(cost_new_iter>cost_ceil)
+                        curr_AA = [];
+                        mAA = [];
+                        asgn_WAidx = [];
+                        cost_currAA = [];
+                    end
+                    break
+                end
+            end
+            
+            disp(cost_currAA)
+            selAA{b,st} = [selAA{b,st}; curr_AA];
+            selWA{b,st} = [selWA{b,st}; curr_WA(asgn_WAidx)];
+            sel_mAA{b} = [sel_mAA{b}; mAA];
+            sel_mWA{b} = [sel_mWA{b}; mWA(asgn_WAidx,:)];
+        end
+    end
+end
+
+end
+
---
+++ ABCD_split_folds.m
@@ -0,0 +1,100 @@
+function [fold_subj, fold_sites] = ABCD_split_folds(subjects, site, fam_id, Nfolds)
+
+if(~exist('Nfolds', 'var') || isempty(Nfolds))
+    Nfolds = 10;
+end
+
+N = length(subjects);
+N_fold = round(N / Nfolds);
+
+if(size(subjects,2) > 1)
+    subjects = subjects';
+end
+
+%% check if the subjects from the same family spread across sites
+uniq_fam = unique(fam_id);
+sites_same_fam = [];
+fam_sz = zeros(length(uniq_fam), 1);
+for f = 1:length(uniq_fam)
+    idx = find(fam_id == uniq_fam(f));
+    fam_sz(f) = length(idx);
+    if(fam_sz(f)>1)
+        fam_site = site(idx);
+        if(length(unique(fam_site)) > 1)
+            sites_same_fam = [site_same_fam; {unique(fam_site)}];
+            str = strjoin(sprintfc('%d', unique(fam_site)), ',');
+            warning('Sites %s must be put into the same fold.', str);
+        end
+    end
+end
+
+%% if there are subjects from the same family across different sites, combine those sites
+if(~isempty(sites_same_fam))
+    restrictions = cat(1, sites_same_fam{:});
+    for i = 1:length(restrictoins)
+        idx = find(cellfun(@(x)(ismember(restrictions(i), x)), sites_same_fam));
+        sites_same_fam{min(idx)} = cat(1, sites_same_fam{idx});
+        sites_same_fam(idx(2:end)) = [];
+    end
+    
+    for i = 1:length(sites_same_fam)
+        for l = 1:length(sites_same_fam{i})
+            site(site == sites_same_fam{i}(l)) = min(sites_same_fam{i});
+        end
+    end
+end
+
+%% 
+uniq_site = unique(site);
+site_sz = zeros(length(uniq_site), 1);
+site_subj = cell(length(uniq_site), 1);
+for i = 1:length(site_sz)
+    site_sz(i) = length(find(site == uniq_site(i) ));
+    site_subj{i} = subjects(site == uniq_site(i));
+end
+% [~, sort_idx] = sort(site_sz, 'descend');
+
+rng('default')
+rng(1000)
+sort_idx = datasample(1:1:length(site_sz), length(site_sz), 'Replace', false);
+
+used_site = zeros(length(uniq_site), 1);
+fold_sz = zeros(Nfolds,1);
+fold_sites = cell(Nfolds, 1);
+for f = 1:Nfolds
+    for s = 1:length(uniq_site)
+        if(used_site(sort_idx(s))==0)
+            if(fold_sz(f) + site_sz(sort_idx(s)) <= N_fold || (fold_sz(f)==0 && site_sz(sort_idx(s)) > N_fold) )
+                fold_sz(f) = fold_sz(f) + site_sz(sort_idx(s));
+                fold_sites{f} = [fold_sites{f}; uniq_site(sort_idx(s))];
+                used_site(sort_idx(s)) = 1;
+            end
+        end
+    end
+end
+
+sz_dif = N_fold - fold_sz;
+for s = 1:length(uniq_site)
+    if(used_site(sort_idx(s)) == 0)
+        [~,max_f] = max(sz_dif);
+        fold_sz(max_f) = fold_sz(max_f) + site_sz(sort_idx(s));
+        fold_sites{max_f} = [fold_sites{max_f}; uniq_site(sort_idx(s))];
+        used_site(sort_idx(s)) = 1;
+        sz_dif = N_fold - fold_sz;
+    end
+end
+
+fold_subj = cell(Nfolds, 1);
+for f = 1:Nfolds
+    uniq_idx = [];
+    for i = 1:length(fold_sites{f})
+        uniq_idx = [uniq_idx; find(uniq_site == fold_sites{f}(i))];
+    end
+    
+    fold_subj{f} = cat(1, site_subj{uniq_idx});
+    fold_subj{f} = intersect(subjects, fold_subj{f}, 'stable');
+end
+
+
+end
+
