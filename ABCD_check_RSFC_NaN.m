7d6eaf5092ced6c8908a82ed08f38c52c9d0571c
jingwei-li <allhappyliferay@gmail.com>
2020-07-21 21:43:35 +0800

    Add a section to save FC combined across subjects
    Why:
    
    * To generate a #ROI x #ROI x #subjects matrix to feed into KRR code
    
    This change addresses the need by:
    
    *

--- ABCD_check_RSFC_NaN.m
+++ ABCD_check_RSFC_NaN.m
@@ -1,4 +1,11 @@
-function pass_subj = ABCD_check_RSFC_NaN(fmri_dir, subj_list)
+function pass_subj = ABCD_check_RSFC_NaN(fmri_dir, subj_list, outname)
+
+% pass_subj = ABCD_check_RSFC_NaN(fmri_dir, subj_list, outname)
+%
+% Example:
+%   pass_subj = ABCD_check_RSFC_NaN([], ...
+%      '/data/users/jingweil/storage/MyProject/fairAI/ABCD_race/scripts/lists/subjects_pass_rs_pass_pheno.txt', ...
+%      '/data/users/jingweil/storage/MyProject/fairAI/ABCD_race/mat/RSFC/pass_rs_pass_pheno_5351.mat');
 
 if(~exist('fmri_dir', 'var') || isempty(fmri_dir))
     fmri_dir = '/mnt/eql/yeo13/data/ABCD/rs_GSR';
@@ -10,15 +17,24 @@ end
 [subjects, nsub] = CBIG_text2cell(subj_list);
 
 hasnan = logical(zeros(nsub,1));
+corr_mat = [];
 for s = 1:nsub
     RSFC_file = fullfile(fmri_dir, subjects{s}, 'FC_metrics', 'Pearson_r', [subjects{s} ...
         '_rest_mc_skip_residc_interp_FDRMS0.3_DVARS50_bp_0.009_0.08_fs6_sm6_all2all.mat']);
-    load(RSFC_file)
-    hasnan(s) = any(isnan(corr_mat(:)));
-    clear corr_mat
+    curr = load(RSFC_file);
+    hasnan(s) = any(isnan(curr.corr_mat(:)));
+    corr_mat = cat(3, corr_mat, curr.corr_mat);
 end
 
 pass_subj = subjects(~hasnan);
+
+if(exist('outname', 'var') && ~isempty(outname))
+    outdir = fileparts(outname);
+    if(~exist(outdir, 'dir'))
+        mkdir(outdir)
+    end
+    save(outname, 'corr_mat', '-v7.3')
+end
 
 end
 
