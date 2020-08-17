c9394cf6f9288de750736bf2ef67fc2bab8b1e14
jingwei-li <allhappyliferay@gmail.com>
2020-08-18 02:31:28 +0800

    Add matlab+bash wrappers for testing all-AA-trained model on matched AA
    Why:
    
    * "allAA" does not fully cover matched AA due to random selection of AA when #AA > #WA for some sites
    
    This change addresses the need by:
    
    *

---
+++ KRR/ABCD_KRR_test_AllAAmodel_on_matchedAA.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+########################
+# setup for CIRC cluster
+########################
+curr_dir=$(pwd)
+
+cd $work_dir
+
+proj_dir="/data/users/jingweil/storage/MyProject/fairAI/ABCD_race"
+model_dir="$proj_dir/models/KRR/20200721/reg_AgeSexMtIcvPEduc_y_allAA"
+bhvr_ls="$proj_dir/scripts/lists/behavior_list.txt"
+split_dir="$proj_dir/mat/matchANDsplit/20200719"
+split_fstem="_pass_rs_pass_pheno"
+AA_subfolder="allAA"
+
+for b in $(cat $bhvr_ls); do
+	cmd="matlab -nojvm -nodesktop -nodisplay -r \"addpath $DIR; \
+ABCD_KRR_test_AllAAmodel_on_matchedAA([], '$model_dir', \
+'$b', [], [], [], '$split_dir', '$split_fstem', '$AA_subfolder'); \
+exit;\" "
+	echo $cmd
+	work_dir="$model_dir/$b/logs"
+	cd $work_dir
+	echo $work_dir
+	echo $cmd | $CBIG_SCHEDULER_DIR/qsub -V -q circ-spool -l \
+walltime=02:00:00,mem=12GB -m ae -N test_AllAAmodel_on_matchedAA_${b} 
+	sleep 3s
+done
