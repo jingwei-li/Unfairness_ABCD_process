b90555fa91132bb3931789d6ed736e08fde9f0fc
jingwei-li <allhappyliferay@gmail.com>
2021-05-13 02:31:15 +0800

    Add comment blocks for all scripts that relate to running KRR on whole population
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- KRR/ABCD_KRR_in_subgroup.sh
+++ KRR/ABCD_KRR_in_subgroup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
+# The general script to train kernel ridge regression within a subgroup 
+# (e.g. African Americans) of the whole population. 
+#
 # Jingwei Li, 20200811
 
 DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -23,6 +26,33 @@ main(){
         rm $FC_file
     fi
 }
+
+usage() { echo "
+NAME: 
+	ABCD_KRR_in_subgroup.sh
+
+DESCRIPTION:
+	The general script to train kernel ridge regression within a subgroup (e.g. African Americans) 
+	of the whole population. 
+
+REQUIRED ARGUMENTS:
+	-bhvr_name     <bhvr_name>    : The behavioral measure to be predicted.
+	-full_subj_ls  <full_subj_ls> : Full path of the subject list containing the whole population.
+	-subj_ls       <subj_ls>      : Full path of the subgroup subject list.
+	-subfold_f     <subfold_f>    : The split folds of the subpopulation.
+	-full_FC_file  <full_FC_file> : Functional connectivity .mat file of the whole population 
+	                                (full path).
+	-FC_file       <FC_file>      : Functional connectivity .mat file of the subpopulation (full path).
+	-outdir        <outdir>       : Full path of the kernel ridge regression output directory.
+
+EXAMPLE:
+	$DIR/ABCD_KRR_in_subgroup.sh \\
+	-bhvr_name nihtbx_reading_uncorrected -full_subj_ls <dir_to_lists>/subjects_pass_rs_pass_pheno.txt \\
+	-subj_ls <dir_to_subgrp_lists>/subj_nihtbx_reading_uncorrected.txt \\
+	-subfold_f <dir_to_split>/split_subgroup_nihtbx_reading_uncorrected.mat \\
+	-full_FC_file <dir_to_RSFC>/pass_rs_pass_pheno_5351.mat.mat \\
+	-FC_file <dir_to_subgrp_RSFC>/RSFC_nihtbx_reading_uncorrected.mat -outdir <output_dir>
+" 1>&2; exit 1; }
 
 ##########################################
 # Parse Arguments 
